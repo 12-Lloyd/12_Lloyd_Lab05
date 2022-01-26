2feefb76a70fd49d0d6176f360b414aa54eaf14f
FriedPotatoe <lloydlim1@gmail.com>
2022-01-26 12:01:07 +0800

    coin interaction

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+
+    public int coin;
+    public GameObject coinText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            Debug.Log("touch");
+            Destroy(collision.gameObject);
+            coin += 10 ;
+            coinText.GetComponent<Text>().text = "Score: " + coin;
+        }
+    }
+}
